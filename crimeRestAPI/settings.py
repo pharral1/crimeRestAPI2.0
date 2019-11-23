12984bcf6f824ed005444418a992d984c636318a
root <django@docker>
2019-11-23 17:30:40 +0000

    Fix for CORS headers, allow cross origin requests

--- crimeRestAPI/settings.py
+++ crimeRestAPI/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Crime.apps.CrimeConfig',
     "rest_framework",
-    "rest_framework_swagger"
+    "rest_framework_swagger",
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'crimeRestAPI.urls'
 
